0abc0b12d4ee8a45293ce6ee596fd7176d685eb4
trillz-dev <timorazzy@gmail.com>
2020-05-04 00:54:48 +0100

    Form validation tutorials

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,59 +1,106 @@
 
+// const emailCheck = () => {
+//     const emailAddress = document.getElementById('email').value;
+//     const emailInput = document.getElementById('email');
+//     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+//     if (emailAddress.match(mailFormat)) {
+//         emailInput.classList.remove('error-input');
+//         document.getElementById('emailWarning').classList.add('hidden')
+//     } else if (!emailAddress.match(mailFormat)) {
+//         emailInput.classList.add('error-input');
+//         document.getElementById('emailWarning').classList.remove('hidden')
+//     }
+// }
+
+// const FirstNameInputCheck = () => {
+//     const firstNameValidate = document.getElementById('firstName').value;
+//     const firstNameValidateInput = document.getElementById('firstName');
+//     const firstNameWarning = document.getElementById('firstNameWarning');
+
+//     if (firstNameValidate.length >= 1 ) {
+//         firstNameValidateInput.classList.remove('error-input');
+//         firstNameWarning.classList.add('hidden')
+//     } else if (firstNameValidate.length < 1) {
+//         firstNameValidateInput.classList.add('error-input');
+//         firstNameWarning.classList.remove('hidden')
+//     }
+// }
+
+// const lastNameInputCheck = () => {
+//     const lastNameValidate = document.getElementById('lastName').value;
+//     const lastNameValidateInput = document.getElementById('lastName');
+//     const lastNameWarning = document.getElementById('lastNameWarning');
+
+//     if (lastNameValidate.length >= 1 ) {
+//         lastNameValidateInput.classList.remove('error-input');
+//         lastNameWarning.classList.add('hidden')
+//     } else if (lastNameValidate.length < 1) {
+//         lastNameValidateInput.classList.add('error-input');
+//         lastNameWarning.classList.remove('hidden')
+//     }
+// }
+
+// const passwordInputCheck = () => {
+//     const passwordValidate = document.getElementById('password').value;
+//     const passwordValidateInput = document.getElementById('password');
+//     const passwordWarning = document.getElementById('passwordWarning');
+
+//     if (passwordValidate.length >= 1 ) {
+//         passwordValidateInput.classList.remove('error-input');
+//         passwordWarning.classList.add('hidden')
+//     } else if (passwordValidate.length < 1) {
+//         passwordValidateInput.classList.add('error-input');
+//         passwordWarning.classList.remove('hidden')
+//     }
+// }
+
+
+// const submitCheck = (e) => {
+//     e.preventDefault();
+//     emailCheck();
+//     FirstNameInputCheck();
+//     lastNameInputCheck();
+//     passwordInputCheck();
+// }
+
+// const submitClick = document.getElementById('btn');
+
+// submitClick.addEventListener('click', submitCheck);
+
+
+
 const emailCheck = () => {
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     const emailAddress = document.getElementById('email').value;
     const emailInput = document.getElementById('email');
-    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
+    const emailWarning = document.getElementById('emailWarning')
 
 
     if (emailAddress.match(mailFormat)) {
         emailInput.classList.remove('error-input');
-        document.getElementById('emailWarning').classList.add('hidden')
+        emailWarning.classList.add('hidden');
     } else if (!emailAddress.match(mailFormat)) {
         emailInput.classList.add('error-input');
-        document.getElementById('emailWarning').classList.remove('hidden')
+        emailWarning.classList.remove('hidden');
     }
 }
 
-const FirstNameInputCheck = () => {
-    const firstNameValidate = document.getElementById('firstName').value;
-    const firstNameValidateInput = document.getElementById('firstName');
-    const firstNameWarning = document.getElementById('firstNameWarning');
 
-    if (firstNameValidate.length >= 1 ) {
-        firstNameValidateInput.classList.remove('error-input');
-        firstNameWarning.classList.add('hidden')
-    } else if (firstNameValidate.length < 1) {
-        firstNameValidateInput.classList.add('error-input');
-        firstNameWarning.classList.remove('hidden')
-    }
-}
+const fieldCheck = (valueToCheck) => {
+    const valueToValidate = document.getElementById(valueToCheck).value;
+    const valueToValidateInput = document.getElementById(valueToCheck);
+    const warningElement = `${valueToCheck}Warning`;
 
-const lastNameInputCheck = () => {
-    const lastNameValidate = document.getElementById('lastName').value;
-    const lastNameValidateInput = document.getElementById('lastName');
-    const lastNameWarning = document.getElementById('lastNameWarning');
 
-    if (lastNameValidate.length >= 1 ) {
-        lastNameValidateInput.classList.remove('error-input');
-        lastNameWarning.classList.add('hidden')
-    } else if (lastNameValidate.length < 1) {
-        lastNameValidateInput.classList.add('error-input');
-        lastNameWarning.classList.remove('hidden')
-    }
-}
-
-const passwordInputCheck = () => {
-    const passwordValidate = document.getElementById('password').value;
-    const passwordValidateInput = document.getElementById('password');
-    const passwordWarning = document.getElementById('passwordWarning');
-
-    if (passwordValidate.length >= 1 ) {
-        passwordValidateInput.classList.remove('error-input');
-        passwordWarning.classList.add('hidden')
-    } else if (passwordValidate.length < 1) {
-        passwordValidateInput.classList.add('error-input');
-        passwordWarning.classList.remove('hidden')
+    if (valueToValidate.length >= 1) {
+        valueToValidateInput.classList.remove('error-input');
+        document.getElementById(warningElement).classList.add('hidden')
+    }else if (valueToValidate.length < 1) {
+        valueToValidateInput.classList.add('error-input');
+        document.getElementById(warningElement).classList.remove('hidden')
     }
 }
 
@@ -61,15 +108,13 @@ const passwordInputCheck = () => {
 const submitCheck = (e) => {
     e.preventDefault();
     emailCheck();
-    FirstNameInputCheck();
-    lastNameInputCheck();
-    passwordInputCheck();
+    fieldCheck('firstName');
+    fieldCheck('lastName');
+    fieldCheck('password');
 }
 
 const submitClick = document.getElementById('btn');
 
 submitClick.addEventListener('click', submitCheck);
-
-
 
 
