8a77e76fd97f7dcb88f283a9ba049479bb905e16
trillz-dev <timorazzy@gmail.com>
2020-05-06 00:10:12 +0100

    email validation

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -5,13 +5,21 @@ const emailCheck = () => {
     const emailWarning = document.getElementById('emailWarning');
 
 
-    if (email.address.match(mailFormat)) {
+    if (emailAddress.match(mailFormat)) {
         emailInput.classList.remove('error-input');
         emailWarning.classList.add('hidden');
-    } else if (!email.address.match(mailFormat)) {
+    } else if (!emailAddress.match(mailFormat)) {
         emailInput.classList.add('error-input');
         emailWarning.classList.remove('hidden')
     }
 }
 
 
+const submitCheck = (e) => {
+    e.preventDefault();
+    emailCheck();
+}
+
+const submit = document.querySelector('.btn')
+
+submit.addEventListener('click', submitCheck)
